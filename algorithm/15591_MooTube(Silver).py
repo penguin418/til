a53aa525dea56db484ba0d35fdef38a3216aadae
penguin418 <penguin418@naver.com>
2021-02-05 21:32:38 +0900

    dfs

---
+++ algorithm/15591_MooTube(Silver).py
@@ -0,0 +1,7 @@
+n, q = map(int, sys.stdin.readline())
+pqr = []
+qs = []
+for _ in range(n):
+    pqr.append(list(map(int, sys.stdin.readline())))
+for _ in range(q):
+    qs.append(list(map(int, sys.stdin.readline())))
