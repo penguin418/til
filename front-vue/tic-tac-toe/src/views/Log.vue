a136ad5a3706c8b02aba0ec6624886aa8daec070
penguin418 <penguin418@naver.com>
2021-02-05 21:34:44 +0900

    init

---
+++ front-vue/tic-tac-toe/src/views/Log.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="log">
+    {{ getLog }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Log",
+  computed:{
+    getLog() {
+      return this.$store.getters.getLog;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
