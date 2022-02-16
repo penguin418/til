c007dd1c62a3f27dcd63aadeeb097b009f46c624
penguin418 <penguin418@naver.com>
2022-02-17 00:45:04 +0900

    circuit breaker

--- practice-vertx/01-future-retry/src/test/java/CircuitBreakerTest.java
+++ practice-vertx/01-future-retry/src/test/java/CircuitBreakerTest.java
@@ -1,2 +1,52 @@
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
 public class CircuitBreakerTest {
+    private final Logger log = LoggerFactory.getLogger(CircuitBreakerTest.class.getName());
+
+    @Test
+    public void CircuitBreakerTest(Vertx vertx, VertxTestContext testContext) {
+        CircuitBreakerOptions cbOption = new CircuitBreakerOptions()
+                .setMaxRetries(10)
+                .setTimeout(1_000)
+                .setFallbackOnFailure(true)
+                .setResetTimeout(2_000);
+        CircuitBreaker breaker = CircuitBreaker
+                .create("new-breaker", vertx, cbOption)
+                .openHandler(v -> {
+                    log.info("circuit opened");
+                }).closeHandler(v -> {
+                    log.info("circuit closed");
+                }).halfOpenHandler(v -> {
+                    log.info("circuit half-opened");
+                });
+
+
+        breaker.execute(promise -> {
+            vertx.setTimer(3_000, timerId -> {
+                log.info("times up");
+                try{
+                    promise.complete("complete");
+                }catch (Throwable t){
+                    log.warn(t);
+                }
+            });
+        }).onSuccess(r -> {
+            log.info("success");
+        }).onFailure(f -> {
+            log.error("failed");
+        }).onComplete(c -> {
+            log.info("completed");
+            testContext.completeNow();
+        });
+    }
+
 }
